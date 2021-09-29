abd2064805c1f41ea9ce4bde692ab1515e2587d5
jeffrey ke <jke@linux10610.dc.engr.scu.edu>
2021-09-29 15:29:17 -0700

    Initial commit.

---
+++ unsorted.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "set.h"
+
+int main(int argc, char *argv[]) {
+    return 1;
+}
+
+SET *createSet(int maxElts){
+
+}
+
+void destroySet(SET *sp){
+
+}
+
+int numElements(SET *sp){
+
+}
+
+void addElement(SET *sp, char *elt){
+
+}
+
+void removeElement(SET *sp, char *elt){
+
+}
+
+char *findElement(SET *sp, char *elt){
+
+}
+
+char **getElements(SET *sp){
+    
+}
